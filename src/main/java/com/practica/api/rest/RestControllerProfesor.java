1a8037f70522508f31d9d88d9ca8ce6ec7dd4344
Tony Medina <cayapadevelop@gmail.com>
2025-09-28 13:37:44 +0200

    creacion de endpoint agregar, actualizar y delete

--- src/main/java/com/practica/api/rest/RestControllerProfesor.java
+++ src/main/java/com/practica/api/rest/RestControllerProfesor.java
@@ -3,7 +3,12 @@ package com.practica.api.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +17,7 @@ import com.practica.api.entity.Profesor;
 import jakarta.annotation.PostConstruct;
 
 @RestController
-@RequestMapping("api/profesores")
+@RequestMapping("api")
 public class RestControllerProfesor {
     List<Profesor> profesorList;
     @PostConstruct
@@ -23,18 +28,47 @@ public class RestControllerProfesor {
         profesorList.add(new Profesor(3, "Mario","Castañeda","Doblaje"));
         
     }
-    @RequestMapping("/lista")
+    @GetMapping("/listaProfesores")
     private List<Profesor> listarProfesores(){
         return profesorList;
     }
 
-    @RequestMapping("/busquedaProfesor/{idProfesor}")
+    @GetMapping("/busquedaProfesor/{idProfesor}")
     private Profesor busquedaProfesor(@PathVariable int idProfesor){
         for(Profesor profesor: this.profesorList){
             if(profesor.getId() == idProfesor)
             return profesor;
         }
         return null;
+    }
+
+    @PostMapping("/listaProfesores")
+    private List<Profesor> agregarProfesor(@RequestBody Profesor elProfesor){
+        profesorList.add(elProfesor);
+        return profesorList;
+
+    }
+
+    @PutMapping("/actualizar")
+    private List<Profesor> actualizarList(@RequestBody Profesor actualizarProfesor){
+        for(Profesor profesor: this.profesorList){
+            if(profesor.getId() == actualizarProfesor.getId()){
+                profesor.setNombre(actualizarProfesor.getNombre());
+                profesor.setApellido(actualizarProfesor.getApellido());
+                profesor.setMateria(actualizarProfesor.getMateria());
+               
+            
+
+            }
+            
+        }
+        return profesorList;
+    }
+
+    @DeleteMapping("listaProfesores/{idDelete}")
+    private int eliminarProfesor(@PathVariable int idDelete){
+       this.profesorList.removeIf(dProf -> dProf.getId() == idDelete);
+         return this.profesorList.size();
     }
 
 
